fea7bdd96b802515508c503fbd43fd92aff36f87
Ritviksiddha Penchala <ritviksiddha@gmail.com>
2022-08-06 19:08:12 -0700

    Update q5-6.py

--- q5-6.py
+++ q5-6.py
@@ -2,8 +2,9 @@ from collections import abc
 import csv 
 import os
 import numpy as np
+import re
 
-FILENAME = "ecoli_annotations.tsv"
+FILENAME = "protein_test.tsv"
 
 input = open(FILENAME)
 tsv_file = csv.reader(input, delimiter="\t")
@@ -12,16 +13,39 @@ raw_proteins = []
 proteins = []
 
 for row in tsv_file:
-    protein_name = row[11]
-    strand = row[4]
-    if "ase" in protein_name:
-        for word in protein_name.split(" "):
-            if ("ase" in word):
-                raw_proteins.append(word)
-                proteins = list(set(raw_proteins))
+    raw_proteins.append(row[0])
+
+for word in raw_proteins:
+    for x in word.replace('-',' ').split():
+        if("ase" in x):
+            x="".join(c for c in x if c.isalpha())
+            proteins.append(x[:(x.find("ase")+3)])
+
+proteins.sort()
+
+protein_name = proteins[0]
+protein_count = 0
+output = []
+
+for x in proteins:
+    if protein_name == x:
+        protein_count += 1
+    else:
+        output.append([protein_name, protein_count])
+        protein_name = x
+        protein_count = 1
+
+for x in output:
+    print(x)
+
+with open('q5-6.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(["Protein Name", "Protein Count"])
+
+    # write multiple rows
+    writer.writerows(output)
 
 print(len(proteins))
 print(len(raw_proteins))
-
-for word in proteins:
-    print(word)
